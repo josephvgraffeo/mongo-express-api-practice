b7e01a74f307322e3f19448825019eaf0f4cafe2
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-04 17:28:38 -0500

    updates

--- src/dbConnect.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-dotenv.config();
-
-export function dbConnect() {
-    const client = new MongoClient(process.env.URI);
-    return client.db(process.env.DB);
-};
